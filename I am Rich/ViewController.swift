c7f8d769846828b6cc78c26f78f6ca9c971ec79b
DexProtege <dwipmakwana@gmail.com>
2020-05-17 10:48:05 -0700

    done

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Dwip Makwana on 2/27/1399 AP.
+//  Copyright © 1399 Dwip Makwana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
